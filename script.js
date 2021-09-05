84f7e744359d8c1087377fc48794e40c7224f11b
Dharan <rdharan7596@gmail.com>
2021-09-05 18:07:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@
             if(domElement!=null && domElement.getAttribute("page-subtype") == "home")
             {
                 domElement.style = "color: grey; display: flex; font-size: 20px; color: grey; display: flex; font-size: 20px;margin-left: 65%;margin-top: 19%;"
-                domElement.innerHTML = "Welcome Buddy !! <br> Home page blocked due to Content manipulation";
+                domElement.innerHTML = "Welcome Buddy !! <br> Home page blocked due to Content manipulation <br> Change it back by disabling Tampermonkey script thala!!";
             }
         }
     }
